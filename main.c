6f001fcc18a3774e422cb8e454d4393f4c5927ba
Jessica Lewinter <jessicalewinter@gmail.com>
2018-10-01 23:35:17 -0300

    Adding comments to error.c and main.c

--- main.c
+++ main.c
@@ -5,6 +5,18 @@
 #include "error.h"
 #include "graham_scan.h"
 
+/* Função: Mostra o ouput das entradas pre-definidas
+ * 
+ * Entrada : argc -> numero de argumentos apontados por argv 
+ *           argv -> vetor de argumentos
+ * Retorno : 0
+ * Saída   : Exibe os pontos que formam o fecho convexo. Caso não existam, exibe uma mensagem
+ *           informando que não é possível formar um fecho com os pontos passados
+ * Variáveis Locais: Nenhuma
+ * Variáveis Globais Usadas : Nenhuma
+ * Variáveis Globais Alteradas : Nenhuma
+ */
+
 int main(int argc, char *argv[])
 {   
     if (argc != 2) {
